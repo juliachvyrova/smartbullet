13b99cb173c2b035be00f9b7d0242ae4568d54e0
pozdnyshevmax <max_rash@mail.ru>
2014-11-11 18:57:34 +0700

    First migration =)

---
+++ protected/migrations/m141111_114612_add_chat.php
@@ -0,0 +1,37 @@
+<?php
+
+class m141111_114612_add_chat extends CDbMigration
+{
+	public function up()
+	{
+            $this->createTable('chatmsg', array(
+            'id' => 'pk',
+            'text' => 'text',
+            'author_id' => 'int(11) not null',
+            'game_id' => 'int(11) not null',
+            ));
+            
+            $this->createTable('game', array(
+            'id' => 'pk', 
+            'game_status' => 'int(2) not null',
+            ));
+            
+	}
+
+	public function down()
+	{
+		echo "m141111_114612_add_chat does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
