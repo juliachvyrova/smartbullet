80d90cd5c4ed878752d4a8415f6b3e32e1f910ed
pozdnyshevmax <max_rash@mail.ru>
2014-11-12 15:44:16 +0700

    1. polling -> new method of controller 2. delete all useless

--- protected/controllers/GameController.php
+++ protected/controllers/GameController.php
@@ -19,11 +19,6 @@ class GameController extends Controller
 		);
 	}
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
 	public function accessRules()
 	{
 		return array(
@@ -45,111 +40,49 @@ class GameController extends Controller
 		);
 	}
  
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
 	public function actionView($id)
 	{
             //Yii::app()->user->id;
             if(Yii::app()->request->isAjaxRequest){ //if ajax 
-                    $textmsg = $_POST['msg'];
-                   
-                    $model = $this->loadModel($id);
-                    if($textmsg != ''){ //add messege
-                        $user_id = $_POST['user_id'];
-                        $msg = new Chatmsg();
-                        $msg->author_id = $user_id;
-                        $msg->text = $textmsg;
-                        $msg->game_id = $model->id;
-                        $msg->save();
-                    }
-                    $str = '';
-                    $model = $this->loadModel($id);
-                    foreach ($model->chatmsg as $chat){ //view all chat messeges
-                            if($chat->author <> NULL){
-                                 $str.= '<span class="user_name">'.$chat->author->login.'</span>: ';
-                                 $str.= $chat->text.'<br>';
-                           }
-                    } 
-                    if (isset($_POST['polling'])){ //if myscript.js calling
-                        echo json_encode(array(
-                            'result' => $str
-                        ));
-                    }else   echo $str;
-                    Yii::app()->end();
+                    $this->polling($id);
                 }
             else {
 		$this->render('view',array( //if it's first enter
 			'model'=>$this->loadModel($id),
 		));
             }
+            Yii::app()->end();
 	}
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new Game;
+	protected function polling($id){
+            
+               $textmsg = $_POST['msg'];
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+               //$model = $this->loadModel($id);
+               if($textmsg != ''){ //add messege
+                   $user_id = $_POST['user_id'];
+                   $msg = new Chatmsg();
+                   $msg->author_id = $user_id;
+                   $msg->text = $textmsg;
+                   $msg->game_id = $id;
+                   $msg->save();
+               }
+               $str = '';
+               $model = $this->loadModel($id);
+               foreach ($model->chatmsg as $chat){ //view all chat messeges
+                       if($chat->author <> NULL){
+                            $str.= '<span class="user_name">'.$chat->author->login.'</span>: ';
+                            $str.= $chat->text.'<br>';
+                      }
+               } 
+               if (isset($_POST['polling'])){ //if myscript.js calling
+                   echo json_encode(array(
+                       'result' => $str,
+                   ));
+               }else   echo $str;   
+        }
 
-		if(isset($_POST['Game']))
-		{
-			$model->attributes=$_POST['Game'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
-
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['Game']))
-		{
-			$model->attributes=$_POST['Game'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
-
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
-
-	/**
-	 * Lists all models.
-	 */
 	public function actionIndex()
 	{
 		$dataProvider=new CActiveDataProvider('Game');
@@ -161,28 +94,7 @@ class GameController extends Controller
             
 	}
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Game('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Game']))
-			$model->attributes=$_GET['Game'];
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Game the loaded model
-	 * @throws CHttpException
-	 */
 	public function loadModel($id)
 	{
 		$model=Game::model()->findByPk($id);
@@ -191,23 +103,12 @@ class GameController extends Controller
 		return $model;
 	}
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param Game $model the model to be validated
-	 */
+
 	protected function performAjaxValidation($model)
 	{
 		if(isset($_POST['ajax']) && $_POST['ajax']==='game-form')
 		{
 			echo CActiveForm::validate($model);
-                  /*  $str = new String();
-                    foreach ($model->chatmsg as $chat){
-                            if($chat->author <> NULL){
-                                 $str.= '<span class="user_name">'.$chat->author->login.'</span>: ';
-                                 $str.= $chat->text.'<br>';
-                           }
-                    }
-                        echo $str;*/
 			Yii::app()->end();
 		}
 	}
