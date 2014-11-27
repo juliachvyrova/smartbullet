98fa0893372f6d0f87bba2662cbede1f9f172913
pozdnyshevmax <max_rash@mail.ru>
2014-11-27 13:23:05 +0700

    game-player - 92%

--- protected/views/game/_view.php
+++ protected/views/game/_view.php
@@ -1,13 +1,15 @@
 <ul>
     
-    <?php if($data->game_status == 0): ?>
      <b><?php echo CHtml::encode($data->getAttributeLabel('id')) ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)) ?>
 	<br />
-
 	<b><?php echo CHtml::encode($data->getAttributeLabel('game_status')) ?>:</b>
-	<?php echo CHtml::encode($data->game_status) ?>
+	<?php 
+        if($data->game_status == 0 )
+            echo 'Open';
+        if($data->game_status == 0 )
+            echo 'Close';
+        ?>
 	<br />
-   <?php endif; ?>
     
 </ul>
