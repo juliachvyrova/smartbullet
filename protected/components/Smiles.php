dc97e2f83b404af4d76ce464e8e92ec9bc6c2d1b
Your Name <you@example.com>
2014-11-27 19:12:19 +0600

    server

--- protected/components/Smiles.php
+++ protected/components/Smiles.php
@@ -3,7 +3,7 @@
 class Smiles{
 
 
-    public function codeToImg($text)
+    public static function codeToImg($text)
 	{	 
 		$location=$location=Yii::app()->baseUrl."/images/smiles/";
 		$typeSmile= array(':)',':(',':D',':P',':x',':*','*sleep*','*angry*','XD','._.',';)','B)');
@@ -15,10 +15,10 @@ class Smiles{
 	    return $text;
 	}
 
-	public function show($textFild)
+	public static function show($textFild)
 	{	 
 		$text="";
-		$location=$location=Yii::app()->baseUrl."/images/smiles/";
+		$location=$location=Yii::app()->baseUrl."../images/smiles/";
 		$typeSmile= array(':)',':(',':D',':P',':x',':*','*sleep*','*angry*','XD','._.',';)','B)');
 	 	$imgSmile=array('smile.png','sad.png','happy.png','tongue_happy.png','silence.png','kiss.png','sleep.png','angry.png','laugh.png','hm.png','ok.png','glas.png');
 	    for ($i=0; $i<count($imgSmile);$i++)
