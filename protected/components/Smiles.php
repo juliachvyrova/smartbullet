ca3c2d43342e3ec89a4c41a06042394e33f8d221
pozdnyshevmax <max_rash@mail.ru>
2014-11-28 14:29:50 +0700

    smile

--- protected/components/Smiles.php
+++ protected/components/Smiles.php
@@ -1,11 +1,6 @@
 <?php
 
 class Smiles{
-<<<<<<< HEAD
-=======
-
-
->>>>>>> d87f585175c4cb925a4020237c7d947ab68a58b8
     public static function codeToImg($text)
     {	 
             $location=$location=Yii::app()->baseUrl."/images/smiles/";
@@ -16,7 +11,6 @@ class Smiles{
 	        $text=str_replace($typeSmile[$i],'<img class=smile src='.$location.$imgSmile[$i]." />", $text);      
 	    }
 	    return $text;
-<<<<<<< HEAD
     }
 
     public static function show($textFild)
@@ -33,20 +27,3 @@ class Smiles{
     }
 
 }
-=======
-	}
-
-	public static function show($textFild)
-	{	 
-		$text="";
-		$location=$location=Yii::app()->baseUrl."../images/smiles/";
-		$typeSmile= array(':)',':(',':D',':P',':x',':*','*sleep*','*angry*','XD','._.',';)','B)');
-	 	$imgSmile=array('smile.png','sad.png','happy.png','tongue_happy.png','silence.png','kiss.png','sleep.png','angry.png','laugh.png','hm.png','ok.png','glas.png');
-	    for ($i=0; $i<count($imgSmile);$i++)
-	    {
-	        $text.=' <img class=smile2 src='.$location.$imgSmile[$i]." onclick=writeSmile('".$typeSmile[$i]."',"."'".$textFild."'".") /> ";      
-	    }
-	    return $text;
-	}
-}
->>>>>>> d87f585175c4cb925a4020237c7d947ab68a58b8
